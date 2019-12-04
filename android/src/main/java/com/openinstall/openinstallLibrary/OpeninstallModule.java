9166b9839b931a0d8c76fc55d5010eaa31decfdf
wenki <946366658@qq.com>
2019-12-04 15:44:42 +0800

    更新android后台运行时，拉起参数还原问题

--- android/src/main/java/com/openinstall/openinstallLibrary/OpeninstallModule.java
+++ android/src/main/java/com/openinstall/openinstallLibrary/OpeninstallModule.java
@@ -5,46 +5,64 @@ import android.content.Intent;
 import android.text.TextUtils;
 import android.util.Log;
 
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.fm.openinstall.OpenInstall;
 import com.fm.openinstall.listener.AppInstallAdapter;
 import com.fm.openinstall.listener.AppWakeUpAdapter;
 import com.fm.openinstall.model.AppData;
 
-public class OpeninstallModule extends ReactContextBaseJavaModule{
-    ReactContext  context;
+public class OpeninstallModule extends ReactContextBaseJavaModule {
+    ReactContext context;
 
     public OpeninstallModule(final ReactApplicationContext reactContext) {
         super(reactContext);
         context = reactContext;
+        reactContext.addActivityEventListener(new ActivityEventListener() {
+            @Override
+            public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+
+            }
+
+            @Override
+            public void onNewIntent(Intent intent) {
+                Activity currentActivity = getCurrentActivity();
+                if (currentActivity != null) {
+                    Log.d("OpenInstallModule", "onNewIntent" );
+                    currentActivity.setIntent(intent);
+                }
+            }
+        });
     }
 
     @ReactMethod
-    public void getWakeUp(final Callback successBack){
-         Activity currentActivity = getCurrentActivity();
-         Intent intent= currentActivity.getIntent();
-          OpenInstall.getWakeUp(intent, new AppWakeUpAdapter() {
-              @Override
-              public void onWakeUp(AppData appData) {
-                  if (appData!=null) {
-                      Log.d("OpenInstallModule", "getWakeUp : wakeupData = " + appData.toString());
-                      String channel = appData.getChannel();
-                      String data = appData.getData();
-                      WritableMap params = Arguments.createMap();
-                      params.putString("channel", channel);
-                      params.putString("data", data);
-                      successBack.invoke(params);
-                  }
-              }
-          });
+    public void getWakeUp(final Callback successBack) {
+        Log.d("OpenInstallModule", "getWakeUp" );
+        Activity currentActivity = getCurrentActivity();
+        if (currentActivity != null) {
+            Intent intent = currentActivity.getIntent();
+            OpenInstall.getWakeUp(intent, new AppWakeUpAdapter() {
+                @Override
+                public void onWakeUp(AppData appData) {
+                    if (appData != null) {
+                        Log.d("OpenInstallModule", "getWakeUp : wakeupData = " + appData.toString());
+                        String channel = appData.getChannel();
+                        String data = appData.getData();
+                        WritableMap params = Arguments.createMap();
+                        params.putString("channel", channel);
+                        params.putString("data", data);
+                        successBack.invoke(params);
+                    }
+                }
+            });
+        }
+
     }
 
     @Override
@@ -59,35 +77,37 @@ public class OpeninstallModule extends ReactContextBaseJavaModule{
     }
 
     @ReactMethod
-    public void getInstall(Integer time,final Callback  callback ){
-
+    public void getInstall(Integer time, final Callback callback) {
+        Log.d("OpenInstallModule", "getInstall" );
         OpenInstall.getInstall(new AppInstallAdapter() {
             @Override
             public void onInstall(AppData appData) {
                 try {
-                        Log.d("OpenInstall", "getInstall : data = " + appData.toString());
-                        String channelCode = appData.getChannel();
-                        String data = appData.getData();
-                        WritableMap params = Arguments.createMap();
-                        params.putString("channel",channelCode);
-                        params.putString("data",data);
-                        callback.invoke(params);
-                }catch (Exception e){
+                    Log.d("OpenInstallModule", "getInstall : data = " + appData.toString());
+                    String channelCode = appData.getChannel();
+                    String data = appData.getData();
+                    WritableMap params = Arguments.createMap();
+                    params.putString("channel", channelCode);
+                    params.putString("data", data);
+                    callback.invoke(params);
+                } catch (Exception e) {
                     callback.invoke(e);
                 }
             }
-        },time*1000);
+        }, time * 1000);
     }
 
     @ReactMethod
-    public void reportRegister(){
+    public void reportRegister() {
+        Log.d("OpenInstallModule", "reportRegister" );
         OpenInstall.reportRegister();
     }
 
     @ReactMethod
-    public void reportEffectPoint(String pointId,Integer pointValue){
-      if (!TextUtils.isEmpty(pointId) && pointValue>=0){
-          OpenInstall.reportEffectPoint(pointId,pointValue);
-      }
+    public void reportEffectPoint(String pointId, Integer pointValue) {
+        Log.d("OpenInstallModule", "reportEffectPoint" );
+        if (!TextUtils.isEmpty(pointId) && pointValue >= 0) {
+            OpenInstall.reportEffectPoint(pointId, pointValue);
+        }
     }
 }
