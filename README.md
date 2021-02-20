33a9d233fba8137f1a5481613bb988aa128da692
wade <hui@openinstall.io>
2021-02-20 16:05:50 +0800

    文档更新，Android适配广告API

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Openinstall React Native Plugin
 openinstall-react-native 是openinstall官方开发的 React Native 插件，使用该插件可以方便快速地集成openinstall相关功能。
 
+**针对使用了 渠道统计 功能中的 广告渠道 效果监测功能的集成，需要参考 [补充文档](#ad)**
+
+**从1.3.0开始，插件将不再自动初始化，需要用户手动调用初始化接口**
 
 # 一、使用 npm 安装插件
 
@@ -61,25 +64,32 @@ $(SRCROOT)/../node_modules/openinstall-react-native/ios/RCTOpenInstall
 ````
 
 ## 三、使用指南
-### 1. 快速下载
-如果只需要快速下载功能，无需其它功能（携带参数安装、渠道统计、一键拉起），完成sdk初始化即可（自动集成方式或手动集成方式）
+### 1. 初始化
 
-### 2. 一键拉起
-#### 导入插件
-请react native端入口js文件中（例如App.js文件）导入：
-```
+请在 react native 端入口 js 文件中（例如 `App.js` 文件）导入：
+``` js
 import OpeninstallModule from 'openinstall-react-native'
 ```
-#### 获取拉起参数
-通常在 `componentDidMount` 中监听
+确保用户同意《隐私政策》之后，再初始化 openinstall。参考 [应用合规指南](https://www.openinstall.io/doc/rules.html)
+``` js
+OpeninstallModule.init()
 ```
+**iOS用户使用插件1.3.0之前的版本升级到新版本，需要参考新的手动集成文档修改**
+
+### 2. 快速下载
+如果只需要快速下载功能，无需其它功能（携带参数安装、渠道统计、一键拉起），完成sdk初始化即可（自动集成方式或手动集成方式）
+
+### 3. 一键跳转
+
+通常在 `componentDidMount` 中注册唤醒回调监听
+``` js
 componentDidMount() {
   //该方法用于监听app通过univeral link或scheme拉起后获取唤醒参数
   this.receiveWakeupListener = map => {
      if (map) {
 	   //do your work here
      }        
-   Alert.alert('拉起回调',JSON.stringify(map)) 
+   Alert.alert('唤醒参数', JSON.stringify(map)) 
   } 
   OpeninstallModule.addWakeUpListener(this.receiveWakeupListener)  
 }
@@ -91,25 +101,25 @@ componentWillUnMount() {
 - 第二个函数返回的是map或字典，包含动态安装参数（data）和渠道参数（channel），注意：只有通过渠道二维码或链接安装app后，才会有渠道参数
 - 如果动态安装参数（data）和渠道参数（channel）同时为空，则map返回null
 
-### 3. 携带参数安装（高级版功能）
+### 4. 携带参数安装（高级版功能）
 在需要获取安装参数的位置，导入插件：
 ```
 import OpeninstallModule from 'openinstall-react-native'
 ```
 调用如下方法，可重复获取（理论上可在任意位置获取安装参数）：
-```
+``` js
 OpeninstallModule.getInstall(10, map => {
   if (map) {
     //do your work here
   }        
-  Alert.alert('安装回调',JSON.stringify(map))     
+  Alert.alert('安装参数', JSON.stringify(map))     
 })
 ```
 - 第一个传入的参数为超时时长，一般为10s左右，如果只是在后台默默统计或使用，可以设置更长时间；第二个函数返回的是map或字典，包含动态安装参数（data）和渠道参数（channel），注意：只有通过渠道二维码或链接安装app后，才会有渠道参数
 - 如果动态安装参数（data）和渠道参数（channel）同时为空，则map返回null
 - 对iOS，该方法尽量写在业务场景需要参数的位置调用（在业务场景时，网络一般都是畅通的），例如，可以选择在用户注册成功后调用该方法获取参数，对用户进行奖励。原因是iOS首次安装、首次启动的app，会询问用户获取网络权限，用户允许后SDK才能正常联网去获取参数。如果调用过早，可能导致网络权限还未允许就被调用，导致参数无法及时拿到，误以为参数不存在
 
-### 4. 渠道统计（高级版功能）
+### 5. 渠道统计（高级版功能）
 SDK 会自动完成访问量、点击量、安装量、活跃量、留存率等统计工作。
 
 #### （1）上报注册事件
@@ -126,3 +136,55 @@ OpeninstallModule.reportEffectPoint('effect_test',1)
 ## 四、导出apk/api包并上传
 - 代码集成完毕后，需要导出安装包上传openinstall后台，openinstall会自动完成所有的应用配置工作。  
 - 上传完成后即可开始在线模拟测试，体验完整的App安装/拉起流程；待测试无误后，再完善下载配置信息。  
+
+<a id="ad"></a>
+## 广告平台接入补充文档
+
+**Android 平台**  
+（1）针对广告平台接入，新增配置接口，在调用 init 之前调用。参考 [广告平台对接Android集成指引](https://www.openinstall.io/doc/ad_android.html)
+``` js
+    /**
+    * adEnabled 为 true 表示 openinstall 需要获取广告追踪相关参数，默认为 false
+    * oaid 为 null 时，表示交由 openinstall 获取 oaid， 默认为 null
+    * gaid 为 null 时，表示交由 openinstall 获取 gaid， 默认为 null
+    */
+    OpeninstallModule.config(true, "通过移动安全联盟获取到的 oaid", "通过 google api 获取到的 advertisingId");
+```
+例如： 开发者自己获取到了 oaid，但是需要 openinstall 获取 gaid，则调用代码为
+``` js
+    // f32a09dc-3312-d43e-6583-62fac13f33ae 是通过移动安全联盟获取到的 oaid
+    OpeninstallModule.config(true, "f32a09dc-3312-d43e-6583-62fac13f33ae", null);
+```
+（2）为了精准地匹配到渠道，需要获取设备唯一标识码（IMEI），因此需要做额外的权限申请    
+在 `AndroidMainfest.xml` 配置文件中添加需要申请的权限 `<uses-permission android:name="android.permission.READ_PHONE_STATE"/>`
+
+（3）申请权限并初始化   
+导入包
+``` js
+import { PermissionsAndroid } from 'react-native'
+```
+在 `App.js` 的 `componentDidMount` 方法中初始化，并且在初始化之前申请权限
+``` js
+this.receiveWakeupListener = map => {    
+    Alert.alert('唤醒参数', JSON.stringify(map)) 
+} 
+var permission = PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE;
+PermissionsAndroid.request(permission)
+    .then(granted => {
+        if (granted === PermissionsAndroid.RESULTS.GRANTED){
+            //权限获取成功
+        }else{
+            //权限获取失败
+        }
+        OpeninstallModule.config(true, null, null);
+        OpeninstallModule.init();
+        OpeninstallModule.addWakeUpListener(this.receiveWakeupListener)
+    })
+```
+
+**iOS 平台**  
+使用第三方插件获取iOS idfa 并传给 openinstall 插件进行初始化
+``` js
+var options = {adid: "通过第三方插件获取的 iOS idfa"}
+OpeninstallModule.init(options);
+```
--- android/src/main/java/com/openinstall/openinstallLibrary/OpeninstallModule.java
+++ android/src/main/java/com/openinstall/openinstallLibrary/OpeninstallModule.java
@@ -14,13 +14,24 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.fm.openinstall.Configuration;
 import com.fm.openinstall.OpenInstall;
 import com.fm.openinstall.listener.AppInstallAdapter;
 import com.fm.openinstall.listener.AppWakeUpAdapter;
 import com.fm.openinstall.model.AppData;
 
 public class OpeninstallModule extends ReactContextBaseJavaModule {
-    ReactContext context;
+
+    private static final String TAG = "OpenInstallModule";
+
+    public static final String EVENT = "OpeninstallWakeupCallBack";
+    private ReactContext context;
+    private Intent wakeupIntent = null;
+    private WritableMap wakeupDataHolder = null;
+    private boolean registerWakeup = false;
+    private boolean callInit = false;
+    private boolean initialized = false;
+    private Configuration configuration = null;
 
     public OpeninstallModule(final ReactApplicationContext reactContext) {
         super(reactContext);
@@ -33,52 +44,10 @@ public class OpeninstallModule extends ReactContextBaseJavaModule {
 
             @Override
             public void onNewIntent(Intent intent) {
-                Log.d("OpenInstallModule", "onNewIntent");
+                Log.d(TAG, "onNewIntent");
                 getWakeUp(intent, null);
             }
         });
-    }
-
-    @ReactMethod
-    public void getWakeUp(final Callback successBack) {
-        Log.d("OpenInstallModule", "getWakeUp");
-        Activity currentActivity = getCurrentActivity();
-        if (currentActivity != null) {
-            Intent intent = currentActivity.getIntent();
-            getWakeUp(intent, successBack);
-        }
-
-    }
-
-    private void getWakeUp(Intent intent, final Callback callback) {
-        OpenInstall.getWakeUp(intent, new AppWakeUpAdapter() {
-            @Override
-            public void onWakeUp(AppData appData) {
-                if (appData != null) {
-                    Log.d("OpenInstallModule", "getWakeUp : wakeupData = " + appData.toString());
-                    String channel = appData.getChannel();
-                    String data = appData.getData();
-                    WritableMap params = Arguments.createMap();
-                    params.putString("channel", channel);
-                    params.putString("data", data);
-
-                    if (callback == null) {
-                        getReactApplicationContext()
-                                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                                .emit("OpeninstallWakeupCallBack", params);
-                    } else {
-                        callback.invoke(params);
-                    }
-
-                }
-            }
-        });
-    }
-
-    @Override
-    public void initialize() {
-        super.initialize();
-        OpenInstall.init(context);
     }
 
     @Override
@@ -87,13 +56,108 @@ public class OpeninstallModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
+    public void config(boolean adEnabled, String oaid, String gaid) {
+        Configuration.Builder builder = new Configuration.Builder();
+        builder.adEnabled(adEnabled);
+        builder.oaid(oaid);
+        builder.gaid(gaid);
+        Log.d(TAG, String.format("config adEnabled=%b, oaid=%s, gaid=%s",
+                adEnabled, oaid == null ? "NULL" : oaid, gaid == null ? "NULL" : gaid));
+        configuration = builder.build();
+    }
+
+    @ReactMethod
+    public void init() {
+        callInit = true;
+        OpenInstall.init(context, configuration);
+        initialized();
+    }
+
+    private void initialized() {
+        initialized = true;
+        if (wakeupIntent != null) {
+            OpenInstall.getWakeUp(wakeupIntent, new AppWakeUpAdapter() {
+                @Override
+                public void onWakeUp(AppData appData) {
+                    wakeupIntent = null;
+                    if (appData != null) {
+                        Log.d(TAG, "getWakeUp : wakeupData = " + appData.toString());
+                        String channel = appData.getChannel();
+                        String data = appData.getData();
+                        WritableMap params = Arguments.createMap();
+                        params.putString("channel", channel);
+                        params.putString("data", data);
+                        getReactApplicationContext()
+                                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                                .emit(EVENT, params);
+                    }
+                }
+            });
+        }
+    }
+
+    @ReactMethod
+    public void getWakeUp(final Callback successBack) {
+        Log.d(TAG, "getWakeUp");
+        registerWakeup = true;
+        checkInit();
+        if (wakeupDataHolder != null) {
+            // 调用getWakeUp注册前就处理过拉起参数了(onNewIntent)
+            getReactApplicationContext()
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(EVENT, wakeupDataHolder);
+            wakeupDataHolder = null;
+        } else {
+            Activity currentActivity = getCurrentActivity();
+            if (currentActivity != null) {
+                Intent intent = currentActivity.getIntent();
+                getWakeUp(intent, successBack);
+            }
+        }
+    }
+
+    // 可能在用户调用初始化之前调用
+    private void getWakeUp(Intent intent, final Callback callback) {
+        boolean isValid = OpenInstall.isValidIntent(intent);
+        if (!isValid) { // 如果不是 openinstall 拉起，忽略
+            return;
+        }
+        if (initialized) {
+            OpenInstall.getWakeUp(intent, new AppWakeUpAdapter() {
+                @Override
+                public void onWakeUp(AppData appData) {
+                    if (appData != null) {
+                        Log.d(TAG, "getWakeUp : wakeupData = " + appData.toString());
+                        String channel = appData.getChannel();
+                        String data = appData.getData();
+                        WritableMap params = Arguments.createMap();
+                        params.putString("channel", channel);
+                        params.putString("data", data);
+                        if (registerWakeup) {
+                            getReactApplicationContext()
+                                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                                    .emit(EVENT, params);
+                        } else {
+                            wakeupDataHolder = params;
+                        }
+                    }
+                }
+            });
+        } else {
+            wakeupIntent = intent;
+        }
+    }
+
+
+    @ReactMethod
     public void getInstall(Integer time, final Callback callback) {
-        Log.d("OpenInstallModule", "getInstall");
+        Log.d(TAG, "getInstall");
+        checkInit();
         OpenInstall.getInstall(new AppInstallAdapter() {
             @Override
             public void onInstall(AppData appData) {
                 try {
-                    Log.d("OpenInstallModule", "getInstall : data = " + appData.toString());
+                    Log.d(TAG, "getInstall : data = " + appData.toString());
                     String channelCode = appData.getChannel();
                     String data = appData.getData();
                     WritableMap params = Arguments.createMap();
@@ -109,15 +173,24 @@ public class OpeninstallModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void reportRegister() {
-        Log.d("OpenInstallModule", "reportRegister");
+        Log.d(TAG, "reportRegister");
+        checkInit();
         OpenInstall.reportRegister();
     }
 
     @ReactMethod
     public void reportEffectPoint(String pointId, Integer pointValue) {
-        Log.d("OpenInstallModule", "reportEffectPoint");
+        Log.d(TAG, "reportEffectPoint");
+        checkInit();
         if (!TextUtils.isEmpty(pointId) && pointValue >= 0) {
             OpenInstall.reportEffectPoint(pointId, pointValue);
+        }
+    }
+
+    private void checkInit() {
+        if (!callInit) {
+            Log.d(TAG, "插件从1.3.0开始，提供初始化接口，如未调用初始化调用其它接口，插件内部将使用默认配置初始化");
+            init();
         }
     }
 }
